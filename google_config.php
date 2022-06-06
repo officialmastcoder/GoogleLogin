915c855c9e5290311a256657cd4d26a1b245524c
AHK WEB SOLUTIONS <96109325+ahkwebsolutions@users.noreply.github.com>
2022-06-06 20:55:43 +0530

    Update google_config.php

--- google_config.php
+++ google_config.php
@@ -12,10 +12,10 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('8768278277-4ugubeq93el51dmb9ma0mretfuecl4ii.apps.googleusercontent.com');
+$google_client->setClientId('Your Client Id Here ');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('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');
+$google_client->setClientSecret('Your Secret here ');
 
 //Set the OAuth 2.0 Redirect URI
 $google_client->setRedirectUri('http://localhost/WinnerDashboard/Landing/Administrator/google_login_controller.php');
